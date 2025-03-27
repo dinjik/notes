4e781a9d0447d3fe9afa4b4f69073b1b517d0667
Eren <eeren1@myseneca.ca>
2025-03-27 00:35:10 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "dinjik.github.io/notes",
+    baseUrl: "dinjik.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
